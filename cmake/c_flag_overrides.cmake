c47f5766644eb3fa8f18d782998c308f063f4305
Tomasz Walczyk <39962135+tomasz-walczyk@users.noreply.github.com>
2019-03-28 23:23:43 +0100

    Build with a static runtime

---
+++ cmake/c_flag_overrides.cmake
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2019 Tomasz Walczyk
+#
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE file for details.
+#
+###########################################################
+
+if(MSVC)
+  set(CMAKE_C_FLAGS_DEBUG_INIT "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
+  set(CMAKE_C_FLAGS_MINSIZEREL_INIT "/MT /O1 /Ob1 /D NDEBUG")
+  set(CMAKE_C_FLAGS_RELEASE_INIT "/MT /O2 /Ob2 /D NDEBUG")
+  set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
+endif()
